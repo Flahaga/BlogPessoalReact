4f4528287e50980a9f9c6c1a51628125b68a7b93
Flah_aga <fl.lindinha2014@gmail.com>
2023-04-11 11:45:49 -0300

    Atualizações

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+return (
+    <>
+        <h1 className='titulo'>Home</h1>
+<img src='https://flammo.com.br/app/uploads/2017/01/58425-o-que-um-site-deve-ter-confira-x-elementos-essenciais-768x721.jpg' alt='Foto da Tela Inicial' className='img'/>
+    </>
+        
+    
+);
+
+}
+
+export default Home;
