23622cc04b990c33ded470a3eb06bd0afcf34f0c
Flah_aga <fl.lindinha2014@gmail.com>
2023-04-13 14:00:01 -0300

    Atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import {Grid} from '@material-ui/core'
 import Home from './paginas/home/Home';
-<source />
 import './App.css'
 
 function App() {
   return (
-  < Home />
+  <>
+      <Navbar />
+      <Home />
+      <Footer />
+  </> 
   );
 }
   export default App;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from'@mui/material';
 import './Home.css';
 
-function Home(){
-return (
-    <>
-        <h1 className='titulo'>Home</h1>
-<img src='https://flammo.com.br/app/uploads/2017/01/58425-o-que-um-site-deve-ter-confira-x-elementos-essenciais-768x721.jpg' alt='Foto da Tela Inicial' className='img'/>
-    </>
-        
-    
-);
-
+function Home() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='box'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)! </Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>Expresse aqui os seus pensamentos e opiniões! </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver  Postagens</Button >
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt='' width="500px" height="500px"/>
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
+
 
 export default Home;
