d5396fc7758e59b2b0381869e126edd51c1e7e29
Flah_aga <fl.lindinha2014@gmail.com>
2023-04-19 18:31:05 -0300

    Atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css'
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/posts" element={<ListaPostagem />} />
           </Routes>
         </div>
         <Footer />
