00fb7ebe1e6d1aa5cbe95c41e00e0d53afcd42a7
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2023-05-03 15:39:46 -0300

    Feat.: Trocando img

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -49,8 +49,7 @@ function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    {/* <img src="https://cdn5.colorir.com/desenhos/color/201533/um-computador-portatil-a-casa-o-quarto-pintado-por-biankamelo-1126111.jpg" alt='' width="500px" height="500px"/> */}
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                    <img src="https://cdn5.colorir.com/desenhos/color/201533/um-computador-portatil-a-casa-o-quarto-pintado-por-biankamelo-1126111.jpg" alt='' width="500px" height="500px"/>
                 </Grid>
                 <Grid xs={12} className='postagens'>
                     <TabPostagem />
