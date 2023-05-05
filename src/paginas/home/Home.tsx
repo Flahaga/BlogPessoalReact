ec0bd55d83b44ff877f23a6198f5ed0c266eb120
Flah_aga <fl.lindinha2014@gmail.com>
2023-05-05 10:07:42 -0300

    imagens

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -48,7 +48,7 @@ function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://imgur.com/C4HzHw1" alt='' width="500px" height="500px"/>
+                    <img src="https://i.imgur.com/C4HzHw1.png" alt='' width="500px" height="500px"/>
                 </Grid>
                 <Grid xs={12} className='postagens'>
                     <TabPostagem />
